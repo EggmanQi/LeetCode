00e9b19d533483c3ce09527d040bd6f858c0cd48
edwin.qi <edwin.qi@vipshop.com>
2018-07-17 16:40:00 +0800

    更新"买卖股票最佳时机2"答案
    修改为贪心算法

--- 买卖股票的最佳时机2.playground/Contents.swift
+++ 买卖股票的最佳时机2.playground/Contents.swift
@@ -1,6 +1,3 @@
-// 找到临时的最小值
-// 比较相邻的值, 大于则比较下一个, 直到相邻的值小于前一个
-
 import UIKit
 
 var str = "Hello, playground"
@@ -12,40 +9,17 @@ func maxProfit(_ prices: [Int]) -> Int {
     }
     
     var result = 0
-    var min = 0
-    var max = 0
-    var next = 1
-    var isBuy = false
+    var temp = 0
     
-    for i in 0..<prices.count {
-        
-        if next>=prices.count {
-            if max>0 || min>0 {
-                result = result + max-min
-            }
-            break
+    for i in 1..<prices.count {
+        temp = prices[i]-prices[i-1]
+        if temp>0 {
+            result = result + temp
         }
-        
-        if prices[i] < prices[next] {
-            
-            if isBuy==false {
-                min = prices[i]
-            }
-            max = prices[next]
-            
-            isBuy = true
-        }else if prices[i] > prices[next] {
-            result = result + (max-min);
-            max=0
-            min=0
-            isBuy = false
-        }
-        
-        next = next + 1
     }
     
     return result
 }
 
-var arr:Array = [7,1,5,3,6,4]//[2,1,2,1,1,0,1,2]
+var arr:Array = [2,1,2,1,1,0,1,2]
 print(maxProfit(arr))
